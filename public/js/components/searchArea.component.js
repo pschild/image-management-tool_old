cff2df82c958ced61c09bc327ace9fe71af1b89a
Schild <Philippe.Schild@adesso.de>
2016-06-02 17:08:55 +0200

    started implementation of new search form

---
+++ public/js/components/searchArea.component.js
@@ -0,0 +1,10 @@
+imt.component('searchArea', {
+    bindings: {
+        title: '@',
+        model: '='
+    },
+    templateUrl: 'views/search/searchArea.html',
+    controller: function SearchAreaController() {
+
+    }
+});
--- public/js/controllers/search.controller.js
+++ public/js/controllers/search.controller.js
@@ -1,2 +1,20 @@
 imt.controller('SearchController', function ($scope) {
+    $scope.models = {
+        image: {
+            name: 'image',
+            fields: [
+                { name: 'name', type: 'text' },
+                { name: 'comment', type: 'text' },
+                { name: 'shotAt', type: 'date' },
+                { name: 'createdAt', type: 'date' }
+            ]
+        },
+        person: {
+            name: 'person',
+            fields: [
+                { name: 'name', type: 'text' },
+                { name: 'birthday', type: 'date' }
+            ]
+        }
+    };
 });
